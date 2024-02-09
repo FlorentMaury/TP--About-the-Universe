b8aa15bbe403557a216f76e9142bd418c7e62e13
Florent Maury <contact@florent-maury.fr>
2024-02-09 16:05:13 +0100

    Ajout de la correction.

--- pages/planets/script.js
+++ pages/planets/script.js
@@ -1,10 +1,24 @@
-// Variables des URLs.
+// Variables.
 let urlPlanets = "https://swapi.dev/api/planets";
+let allPlanets = [];
 
 // Variables des éléments du DOM.
 let table       = document.querySelector('table');
 let planetInfos = document.querySelector('#planetInfos');
 document.querySelector('#planetRequest').style.display = 'none';
+
+// Fonction pour récupérer toutes les planètes au démarrage de l'application.
+async function fetchAllPlanets() {
+    allPlanets = await getAllPlanets();
+}
+
+// Fonction d'initialisation.
+async function onInit() {
+    await fetchAllPlanets();
+    let filteredPlanets = filterPlanets('1');
+    clearTable(table);
+    displayPlanets(filteredPlanets, table);
+}
 
 // Fonction qui permet de récupérer le nombre de pages de planètes.
 async function getPagesNumber() {
@@ -35,7 +49,7 @@ async function getAllPlanets() {
 }
 
 // Fonction pour filtrer les planètes en fonction de la population.
-function filterPlanets(allPlanets, filterValue) {
+function filterPlanets(filterValue) {
     let filteredPlanets = [];
 
     switch (filterValue) {
@@ -98,18 +112,91 @@ function displayPlanetDetails(planet) {
     document.querySelector('#planetRequest').style.display = 'block';
 }
 
-// Fonction principale qui utilise les fonctions ci-dessus.
-async function allPlanets(filterValue) {
-    let allPlanets = await getAllPlanets();
-    let filteredPlanets = filterPlanets(allPlanets, filterValue);
-    clearTable(table);
-    displayPlanets(filteredPlanets, table);
-}
-
 document.querySelector('#populationFilter').addEventListener('change', (event) => {
     let value = event.target.value;
-    allPlanets(value);
+    let filteredPlanets = filterPlanets(value);
+    clearTable(table);
+    displayPlanets(filteredPlanets, table);
 });
 
 // Allumage !
-allPlanets('1');
+onInit();
+
+
+
+
+
+
+        // CORRECTION.
+
+        
+// // Variables globales
+// let planets = [];
+
+// // OnInit : exécuté dès le chargement
+// onInit();
+// async function onInit() {
+//     const countPages = await getCountPages();
+//     await getPlanets(countPages);
+//     showPlanets();
+//     showCountPlanets();
+// };
+
+// // Fonctions ---------------------------
+// async function getCountPages() {
+//     const response = await fetch("https://swapi.dev/api/planets");
+//     const planetsInfos = await response.json();
+//     return planetsInfos.count / 10;
+// };
+
+// async function getPlanets(countPages) {
+//     const url = "https://swapi.dev/api/planets/?page="
+//     for (let i = 1; i <= countPages; i++) {
+//         const response = await fetch(url + i);
+//         const planetsInfos = await response.json();
+//         planets.push(...planetsInfos.results);
+//     };
+// };
+
+// function showPlanets() {
+//     const tbody = document.querySelector('.table tbody');
+//     planets.forEach(planet => {
+//         const tr = document.createElement('tr');
+//         const td1 = document.createElement('td');
+//         const td2 = document.createElement('td');
+//         td1.textContent = planet.name;
+//         td2.textContent = planet.climate;
+//         tr.appendChild(td1);
+//         tr.appendChild(td2);
+//         tbody.appendChild(tr);
+//         tr.addEventListener('click', showPlanet);
+//         // tbody.innerHTML += `<tr><td>${planet.name}</td><td>${planet.climate}</td></tr>`;
+//     });
+// };
+
+// function showCountPlanets() {
+//     document.querySelector('.info').textContent = planets.length + ' resultat(s)';
+// };
+
+// function showPlanet(event) {
+//     const namePlanet = 
+//         event.target.parentNode.querySelector('td').textContent;
+//     const planet = planets.find(planet => planet.name == namePlanet);
+//     document.querySelector('.namePlanet').textContent = planet.name;
+//     document.querySelector('.gravityPlanet').textContent = planet.gravity;
+// };
+// // Gestion des erreurs
+
+// fetch("https://swapi.dev/api/planes")
+//     .then(response => {
+//         if (response.ok) {
+//             return response.json()
+//         };
+//         throw new Error("Nous rencontrons un problème")
+//     })
+//     .then(response => {
+//         console.log(response);
+//     })
+//     .catch(err => {
+//         console.log(err);
+//     });
